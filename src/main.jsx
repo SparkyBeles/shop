05d4049c5b35c33535dfa68067bd921c96e33c9f
camillafalk <ccamilla.falk@gmail.com>
2025-10-07 15:12:26 +0200

    trying again...

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Home from "./pages/Home"
-import { configureStore } from '@reduxjs/toolkit'
-import cartReducer from './features/CartSlice.js';
+//import { configureStore } from '@reduxjs/toolkit'
+//import cartReducer from './features/CartSlice.js';
 import { Provider } from 'react-redux';
-import { createBrowserRouter, RouterProvider } from 'react-router';
+//import { createBrowserRouter, RouterProvider } from 'react-router';
 
+/*
 const store = configureStore({
   reducer: {
     cart: cartReducer
@@ -23,11 +24,12 @@ const router = createBrowserRouter([
 
 
 ]);
+*/
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Provider store = {store}>
-    <RouterProvider router={router} />
-    </Provider>
+    {/* <Provider store = {store}> */}
+    {/* <RouterProvider router={router} /> */}
+    {/* </Provider> */}
   </StrictMode>,
 )
