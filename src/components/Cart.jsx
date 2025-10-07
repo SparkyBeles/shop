fec0c4ce413ced6357aa45de86d761df30d0882a
camillafalk <ccamilla.falk@gmail.com>
2025-10-07 14:53:29 +0200

    try again

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,14 +1,13 @@
-/* 
 
-import { useDispatch } from "react-redux"
+
+//import { useDispatch } from "react-redux"
 
 const Cart = () => {
 
-    const dispatch = useDispatch();
+    //const dispatch = useDispatch();
 
-    //
+    
 }
 
 export default Cart;
 
-*/
