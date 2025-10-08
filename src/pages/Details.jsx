67ec412bd2e02654739f4048c8916c4fe1109ba3
camillafalk <ccamilla.falk@gmail.com>
2025-10-08 13:23:33 +0200

    Used Api-fetch to set up Details page. Will remove later when Home page is done. Added media query to change between desktop and mobile.

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,14 +1,77 @@
+import { useEffect, useState } from "react";
 import Menu from "../components/Menu";
+import '../css/Details.css'; 
 
 function Details() {
 
+    const [movie, setMovie] = useState();
 
-    return (
-        <>
-        <h2>Details</h2>
-        <Menu></Menu>
-        </>
-    )
+    const APIKey = import.meta.env.VITE_API_KEY
+    const apiURL = `https://api.themoviedb.org/3/search/movie?api_key=${APIKey}&query=Troy`
+
+    useEffect(() => {
+
+        const fetchMovie = async () => {
+            const response = await fetch(apiURL);
+            const movieData = await response.json();
+
+            const foundMovie = movieData.results.find(m => m.title === "Troy"); 
+            setMovie(foundMovie);
+
+        }
+
+        fetchMovie()
+        console.log('Fetch work...')
+
+    }, [APIKey]);
+
+  return (
+
+    <>
+      <Menu></Menu>
+
+      <div className="movie">
+
+        {movie ? (
+            <>
+         
+        
+
+         <div className="movie-poster">
+
+            {movie.poster_path && (
+
+                <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
+            )}
+        </div>
+        <div className="movie-info">
+
+
+          <h1>{movie.title}  ({movie.release_date?.slice(0 , 4) }) </h1>
+          <p>{movie.overview?.slice(0 , 600)} </p>
+
+          <h2> €29</h2>
+
+          <div className="buy-buttons">
+          
+          <button>Add to cart</button>
+          <button>Buy now</button>
+          </div>
+          
+           
+        </div>
+           </>
+        ) : (
+            <p>Laddar...</p>
+        )
+    }
+
+       
+      </div>
+    
+    </>
+    
+  );
 }
 
 export default Details;
