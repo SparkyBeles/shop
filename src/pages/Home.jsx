02a21cc26e9445876e498cbaea82f0f3603007ad
SparkyBeles <beles.dev@gmail.com>
2025-10-08 23:42:33 +0200

    implement movie fetching and display in Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,38 @@
 import Menu from "../components/Menu";
+import api from "../api/tmdbAPI";
+import { useEffect, useState } from "react";
+import tmdbAPI from "../api/tmdbAPI";
+import "../css/Home.css";
+import MovieCard from "../components/movieCard";
 
+const ImgBase = "https://image.tmdb.org/t/p/w342";
 function Home() {
+  const [items, setItems] = useState([]);
 
+  useEffect(() => {
+    (async () => {
+      const data = await tmdbAPI();
+      setItems(data ?? []);
+    })();
+  }, []);
 
-return(
-    <Menu></Menu>
-);
+  return (
+    <section>
+      <Menu />
+
+      <section className="movie-container">
+        <div className="movie-grid">
+          {items.map((item) => (
+            <MovieCard
+              key={`${item.type}-${item.id}`}
+              poster={item.poster ? `${ImgBase}${item.poster}` : ""}
+              title={item.title}
+              price={Math.floor(Math.random() * 250) + 50}
+            />
+          ))}
+        </div>
+      </section>
+    </section>
+  );
 }
 export default Home;
