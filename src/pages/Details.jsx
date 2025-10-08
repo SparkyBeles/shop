52da2d63506d0259dfa37ce5af07873981a611f8
camillafalk <ccamilla.falk@gmail.com>
2025-10-08 16:18:02 +0200

    Added function to zoom in on poster with dark overlay. Seem to work on all screen sizes.

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,83 +1,86 @@
 import { useEffect, useState } from "react";
 import Menu from "../components/Menu";
-import '../css/Details.css'; 
+import "../css/Details.css";
 import { useNavigate } from "react-router";
 
 function Details() {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const goToCheckout = () => {
+    navigate("/Checkout");
+  };
 
-    const goToCheckout = () => {
-        navigate('/Checkout');
-    }
+  const [movie, setMovie] = useState();
+  const [isZoomed, setIsZoomed] = useState(false);
 
-    const [movie, setMovie] = useState();
+  const zooming = () => {
+    setIsZoomed(!isZoomed);
+  };
 
-    const APIKey = import.meta.env.VITE_API_KEY
-    const apiURL = `https://api.themoviedb.org/3/search/movie?api_key=${APIKey}&query=Troy`
+  const APIKey = import.meta.env.VITE_API_KEY;
+  const apiURL = `https://api.themoviedb.org/3/search/movie?api_key=${APIKey}&query=Up`;
 
-    useEffect(() => {
+  useEffect(() => {
+    const fetchMovie = async () => {
+      const response = await fetch(apiURL);
+      const movieData = await response.json();
 
-        const fetchMovie = async () => {
-            const response = await fetch(apiURL);
-            const movieData = await response.json();
+      const foundMovie = movieData.results.find((m) => m.title === "Up");
+      setMovie(foundMovie);
+    };
 
-            const foundMovie = movieData.results.find(m => m.title === "Troy"); 
-            setMovie(foundMovie);
-
-        }
-
-        fetchMovie()
-        console.log('Fetch work...')
-
-    }, [apiURL]);
+    fetchMovie();
+    console.log("Fetch work...");
+  }, [apiURL]);
 
   return (
-
-    <>
+    <div className="full-screen">
       <Menu></Menu>
 
       <div className="movie">
-
         {movie ? (
-            <>
-         <div className="movie-poster">
+          <>
+            <div className="movie-poster">
+              {movie.poster_path && (
+                <img
+                  src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                  onClick={zooming}
+                />
+              )}
+            </div>
 
-            {movie.poster_path && (
+            <div className="movie-info">
+              <h1>
+                {movie.title} ({movie.release_date?.slice(0, 4)}){" "}
+              </h1>
+              <p>{movie.overview?.slice(0, 400)} </p>
 
-                <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
-            )}
-        </div>
-        <div className="movie-info">
+              <h2> €29</h2>
 
-
-          <h1>{movie.title}  ({movie.release_date?.slice(0 , 4) }) </h1>
-          <p>{movie.overview?.slice(0 , 400)} </p>
-
-          <h2> €29</h2>
-
-          <div className="buy-buttons">
-          
-
-          {/* add to cart, cart-icon change number of items it has */}
-          <button>Add to cart</button> 
-          {/* Buy now, adds to cart and navigate to check out. */}
-          <button id="buy-now-btn" onClick={goToCheckout}>Buy now</button>
-          </div>
-          
-           
-        </div>
-           </>
+              <div className="buy-buttons">
+                {/* add to cart, cart-icon change number of items it has */}
+                <button>Add to cart</button>
+                {/* Buy now, adds to cart and navigate to check out. */}
+                <button id="buy-now-btn" onClick={goToCheckout}>
+                  Buy now
+                </button>
+              </div>
+            </div>
+          </>
         ) : (
-            <p>Laddar...</p>
-        )
-    }
-
-       
+          <p>Laddar...</p>
+        )}
       </div>
-    
-    </>
-    
+
+      {isZoomed && (
+        <div className="overlay" onClick={zooming}>
+          <img
+            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+            className="zoomed-img"
+          />
+        </div>
+      )}
+    </div>
   );
 }
 
