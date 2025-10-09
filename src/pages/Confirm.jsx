2d4c19fe613d824e2dbe3ee9305ec8dd1943dbea
Saeid-hub <iamsaeidahmadi@gmail.com>
2025-10-09 11:06:29 +0200

    Fix: correct import path for Confirm.css

--- src/pages/Confirm.jsx
+++ src/pages/Confirm.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useLocation, Link } from "react-router-dom";
 import Menu from "../components/Menu";
-import "../CSS/Confirm.css"; 
+import "../css/Confirm.css"; 
 
 function Confirm() {
   const location = useLocation();
