e6eadf59c8acb7ab0575ab65b3c823c23eb3ad0e
SparkyBeles <beles.dev@gmail.com>
2025-10-09 23:27:55 +0200

    add cart functionality to Layout component with Sidebar integration

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,17 +1,26 @@
 import { Outlet } from "react-router";
 import Top from "../components/Top";
 import Menu from "../components/Menu";
+import { useState } from "react";
+import Sidebar from "../components/Sidebar";
+import Cart from "./Cart";
 
 
 function Layout () {
+    const [isCartOpen, setIsCartOpen] = useState(false);
+
     return (
         <>
         <header>
-            <Top />
+            <Top  onCartClick={() => setIsCartOpen(true)} />
             <Menu />
         </header>
         <main>
             <Outlet />
+            <Sidebar 
+            open={isCartOpen} 
+            onClose={() => setIsCartOpen(false)} 
+            title="cart"><Cart/></Sidebar>
         </main>
         </>
     )
