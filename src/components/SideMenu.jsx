cc19536307567ddb68fa1634a204bbaebb0b87ff
camillafalk <ccamilla.falk@gmail.com>
2025-10-09 17:11:11 +0200

    Added some style and css to sidemenu.

--- src/components/SideMenu.jsx
+++ src/components/SideMenu.jsx
@@ -5,10 +5,7 @@ const SideMenu = ({ isOpen, toggleSideMenu }) => {
   return (
     <>
       <div className={`side-menu ${isOpen ? "open" : ""}`}>
-
-       
         <nav>
-
           <NavLink
             to="/Details"
             onclick={toggleSideMenu}
@@ -17,79 +14,152 @@ const SideMenu = ({ isOpen, toggleSideMenu }) => {
             Details
           </NavLink>
 
-          <NavLink
-            to="/Details"
-            onclick={toggleSideMenu}
-            className={"nav-button"}
-          >
-            Details
+          <NavLink to="/Cart" onclick={toggleSideMenu} className={"nav-button"}>
+            Cart
           </NavLink>
 
           <NavLink
-            to="/Details"
+            to="/Checkout"
             onclick={toggleSideMenu}
             className={"nav-button"}
           >
-            Details
+            Checkout
           </NavLink>
 
           <NavLink
-            to="/Details"
+            to="/Confirm"
             onclick={toggleSideMenu}
             className={"nav-button"}
           >
-            Details
+            Confirm
           </NavLink>
 
-          <NavLink
-            to="/Details"
-            onclick={toggleSideMenu}
-            className={"nav-button"}
-          >
-            Details
-          </NavLink>
+          <div className="category-container">
+            <NavLink
+              to="/Details"
+              onclick={toggleSideMenu}
+              className={"nav-button-category"}
+            >
+              Action
+            </NavLink>
+            <NavLink
+              to="/Details"
+              onclick={toggleSideMenu}
+              className={"nav-button-category"}
+            >
+              Adventure
+            </NavLink>
+            <NavLink
+              to="/Details"
+              onclick={toggleSideMenu}
+              className={"nav-button-category"}
+            >
+              Animation
+            </NavLink>
+            <NavLink
+              to="/Details"
+              onclick={toggleSideMenu}
+              className={"nav-button-category"}
+            >
+              Comedy
+            </NavLink>
+            <NavLink
+              to="/Details"
+              onclick={toggleSideMenu}
+              className={"nav-button-category"}
+            >
+              Crime
+            </NavLink>
+            <NavLink
+              to="/Details"
+              onclick={toggleSideMenu}
+              className={"nav-button-category"}
+            >
+              Documentary
+            </NavLink>
+            <NavLink
+              to="/Details"
+              onclick={toggleSideMenu}
+              className={"nav-button-category"}
+            >
+              Drama
+            </NavLink>
+            <NavLink
+              to="/Details"
+              onclick={toggleSideMenu}
+              className={"nav-button-category"}
+            >
+              Family
+            </NavLink>
+            <NavLink
+              to="/Details"
+              onclick={toggleSideMenu}
+              className={"nav-button-category"}
+            >
+              Fantasy
+            </NavLink>
 
-          <p>Categories</p>
+            <NavLink
+              to="/Details"
+              onclick={toggleSideMenu}
+              className={"nav-button-category"}
+            >
+              History
+            </NavLink>
 
-          <NavLink
-            to="/Details"
-            onclick={toggleSideMenu}
-            className={"nav-button"}
-          >
-            Details
-          </NavLink>
+            <NavLink
+              to="/Details"
+              onclick={toggleSideMenu}
+              className={"nav-button-category"}
+            >
+              Horror
+            </NavLink>
 
-          <NavLink
-            to="/Details"
-            onclick={toggleSideMenu}
-            className={"nav-button"}
-          >
-            Details
-          </NavLink>
+            <NavLink
+              to="/Details"
+              onclick={toggleSideMenu}
+              className={"nav-button-category"}
+            >
+              Musicals
+            </NavLink>
 
-          <NavLink
-            to="/Details"
-            onclick={toggleSideMenu}
-            className={"nav-button"}
-          >
-            Details
-          </NavLink>
+            <NavLink
+              to="/Details"
+              onclick={toggleSideMenu}
+              className={"nav-button-category"}
+            >
+              Romance
+            </NavLink>
+          </div>
 
-          <NavLink
-            to="/Details"
-            onclick={toggleSideMenu}
-            className={"nav-button"}
-          >
-            Details
-          </NavLink>
-
-          <NavLink
-            to="/Details"
-            onclick={toggleSideMenu}
-            className={"nav-button"}
-          >
-            Details
-          </NavLink>
+          <div className="about-container">
+            <NavLink
+              to="/Checkout"
+              onclick={toggleSideMenu}
+              className={"about-button"}
+            >
+              About
+            </NavLink>
+            <NavLink
+              to="/Checkout"
+              onclick={toggleSideMenu}
+              className={"about-button"}
+            >
+              FAQ
+            </NavLink>
+            <NavLink
+              to="/Checkout"
+              onclick={toggleSideMenu}
+              className={"about-button"}
+            >
+              Contact
+            </NavLink>
+            <NavLink
+              to="/Checkout"
+              onclick={toggleSideMenu}
+              className={"about-button"}
+            ></NavLink>
+          </div>
         </nav>
       </div>
     </>
