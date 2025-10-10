85822703a8552956a36ce6f94824ff2de8ac5893
SparkyBeles <beles.dev@gmail.com>
2025-10-10 21:11:08 +0200

    refac with some comments and added live search with searchbar input

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,19 +2,26 @@ import { useEffect, useState } from "react";
 import tmdbAPI from "../api/tmdbAPI";
 import "../css/Home.css";
 import SideMenu from "../components/SideMenu";
-
 import MovieListCard from "../components/MovieListCard";
 import { Link } from "react-router";
-
+import searchApi from "../api/searchApi";
 
 const ImgBase = "https://image.tmdb.org/t/p/w342";
 
 function Home() {
+  // ======================================================
+  //    STATES
+  // ======================================================
   const [items, setItems] = useState([]);
-
   const [sideMenuOpen, setSideMenuOpen] = useState(false);
+  const [search, setSearch] = useState("");
+  const [isSearching, setIsSearching] = useState(false);
 
   const toggleSideMenu = () => setSideMenuOpen((prev) => !prev);
+
+  // =======================================
+  // API CALLS
+  //========================================
 
   useEffect(() => {
     (async () => {
@@ -23,32 +30,75 @@ function Home() {
     })();
   }, []);
 
-  return (
+  // =======================================
+  // EFFECTS
+  //========================================
 
+  // live search
+  useEffect(() => {
+    if (!search || search.trim() === "") {
+      (async () => {
+        setIsSearching(true);
+        const data = await tmdbAPI();
+        setItems(data ?? []);
+        setIsSearching(false);
+      })();
+      return;
+    }
+
+    // =======================================
+    // FUNCTION
+    //========================================
+
+    (async () => {
+      setIsSearching(true);
+      const results = await searchApi(search);
+      setItems(results);
+      setIsSearching(false);
+    })();
+  }, [search]);
+
+  // =======================================
+  // VIEW
+  //========================================
+
+  return (
     <section>
       <SideMenu isOpen={sideMenuOpen} toggleSideMenu={toggleSideMenu} />
 
       <img src="./menu.svg" alt="menu" onClick={toggleSideMenu} />
 
-      <section className="movie-grid">
+      <section className="searchBar">
+        <div className="search-container">
+          <input
+            type="search"
+            placeholder="Search for movies,series or actor..."
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            className="search-input"
+          />
 
-          {items.map((item) => (
-            <Link to= {{
+        </div>
+        </section>
+
+      <section className="movie-grid">
+        {items.map((item) => (
+          <Link
+            to={{
               pathname: "/details",
-              state: { id: item.id }
-              }}
-              >
+              state: { id: item.id },
+            }}
+          >
             <MovieListCard
               key={`${item.type}-${item.id}`}
               poster={item.poster ? `${ImgBase}${item.poster}` : ""}
               title={item.title}
               price={Math.floor(Math.random() * 250) + 50}
-              />
-            </Link>
-          ))}
-          </section>
+            />
+          </Link>
+        ))}
       </section>
-    
+    </section>
   );
 }
 
