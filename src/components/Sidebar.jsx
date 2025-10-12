955de0c8b15b0d2b522b7c8701a56f3f63ddfe05
ElDavido81 <eldavido81@gmail.com>
2025-10-12 11:55:52 +0200

    cart css, total sum, checkout-state

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import "../css/Sidebar.css";
 import { useSelector } from "react-redux";
+import { Link } from "react-router";
 
 function Sidebar({ open, onClose, children }) {
   useEffect(() => {
@@ -12,6 +13,15 @@ function Sidebar({ open, onClose, children }) {
     return () => window.removeEventListener("keydown", onKey);
   }, [open, onClose]);
 
+  function cartTotal(cart) {
+    let sum = 0;
+    cart.forEach(cartItem => {
+      sum += cartItem.price;
+    });
+    return sum;
+  }
+
+
   const cart = useSelector((state) => state.cart.value);
 
   return (
@@ -19,17 +29,30 @@ function Sidebar({ open, onClose, children }) {
       className={`sidebar ${open ? "is-open" : ""}`}
       aria-hidden={!open}
     >
+
       <button className="close-btn" onClick={onClose}> x </button>
       {children}
+      
+
       {cart.map((cartItem, index) => (
-        <div key={index}>
+        <div className="moviedetails-sidebar" key={index}>
           <img src={cartItem.poster}/>
-          <h2>{cartItem.title}</h2>
-          <p>{cartItem.price}</p>
+          <span className="cart-item-title">{cartItem.title}</span>
+          <span className="cart-item-price">{cartItem.price} kr</span>
           </div>
       ))}
-    </div>
 
+      <div className="cart-total"><span>Total:</span> {cartTotal(cart)} kr
+            <Link to= {{
+              pathname: '/checkout',
+              state: { sum: cartTotal(cart) }
+              }}>
+            <button className="buy_now">
+                <img className="cart_button" src="src/assets/coins.png"></img>
+                 </button>
+            </Link>
+      </div>
+      </div>
   );
 }
 
