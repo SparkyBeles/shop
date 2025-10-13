f0f859027fefd9011f242eff611bd10a1340077b
ElDavido81 <eldavido81@gmail.com>
2025-10-13 12:31:23 +0200

    add to cart css & logic, images moved to public, <a> bug fix

--- src/features/CartSlice.js
+++ src/features/CartSlice.js
@@ -3,7 +3,8 @@ import { createSlice } from "@reduxjs/toolkit"
 
 
 const initialState = {
-    value: []
+    value: [],
+    items: {},
 }
 
 
@@ -12,15 +13,20 @@ const cartSlice = createSlice ({
     initialState,
     reducers: {
         add : (state, action) => {
-            state.value.push(action.payload) 
+            state.value.push(action.payload)
         },
         remove : (state) => {
             state.value.pop()
+        },
+        toggle : (state, action) => {
+            const id = action.payload;
+            state.items[id] = !state.items[id];
         }
     }
 })
 
 export const {add} = cartSlice.actions;
 export const {remove} = cartSlice.actions;
+export const {toggle} = cartSlice.actions;
 
 export default cartSlice.reducer; 
