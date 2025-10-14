7119824eb0b0179eea7dd1322ea36f763edf10d5
SparkyBeles <beles.dev@gmail.com>
2025-10-14 10:42:40 +0200

    fixed spacing

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,18 +1,17 @@
 import { useNavigate } from "react-router-dom";
 import "../css/404.css";
 
-
 function NotFound() {
-    const navigate = useNavigate();
-    const backToHome = () =>{
-        navigate("/");
-    };
+  const navigate = useNavigate();
+  const backToHome = () => {
+    navigate("/");
+  };
   return (
-    <div className="container">
-      <div className="NotFound-container">
-        <h1>Roll credits… this page didn’t make the final edit.</h1>
-        <button className="back-button" onClick={backToHome}>Back to home</button>
-      </div>
+    <div className="NotFound-container">
+      <h1>Roll credits… this page didn’t make the final edit.</h1>
+      <button className="back-button" onClick={backToHome}>
+        Back to home
+      </button>
     </div>
   );
 }
