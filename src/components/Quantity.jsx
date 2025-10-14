c8b5ed52159802781170596d010dc6a82b272904
ElDavido81 <eldavido81@gmail.com>
2025-10-14 17:08:06 +0200

    initial commit increase/decrease

--- src/components/Quantity.jsx
+++ src/components/Quantity.jsx
@@ -3,17 +3,38 @@ import { useSelector } from "react-redux";
 function Quantity({id}) {
 
     const isAdded = useSelector((state) => state.cart.items[id]);
+    
+    const itemInCart = useSelector((state) => 
+        state.cart.value.find((item) => item.id === id)
+        );
+        const itemAmount = itemInCart?.quantity ?? 0;
 
-  const itemInCart = useSelector((state) => 
-    state.cart.value.find((item) => item.id === id)
-);
+    function increaseAmount (amount) {
+    console.log("increase");
+}
+    function decreaseAmount (amount) {
+    console.log("decrease");
+    }
 
-const itemAmount = itemInCart?.quantity ?? 0;
 
 console.log(itemAmount)
 
     return (
-        <span className={isAdded ? 'amount' : 'amount_hidden'}>{itemAmount}</span>
+        <div className="quantity">
+            <img className="arrow" src="arrow_up.png" onClick={(e) => 
+            {
+            e.preventDefault();
+            e.stopPropagation();
+            increaseAmount();
+            }} />
+            <span className={isAdded ? 'amount' : 'amount_hidden'}>{itemAmount}</span>
+            <img className="arrow" src="arrow_down.png" onClick={(e) => 
+            {
+            e.preventDefault();
+            e.stopPropagation();
+            decreaseAmount();
+            }} />
+        </div>
     )
 }
 
