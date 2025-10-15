376c8650084dd1374537c06dba397f1508c6a13e
SparkyBeles <beles.dev@gmail.com>
2025-10-15 15:00:51 +0200

    Add 404 error page with styling

---
+++ src/pages/404.jsx
@@ -0,0 +1,10 @@
+import "../css/404.css"
+
+function notFound(){
+    return(
+        <div className="Error-container"><h1>Roll the Credits: Scene Not Found!</h1>
+        <button>Back to home</button>
+        </div>
+    )
+}
+export default notFound;
